0e2a870addd02f351bffa7d394431b7f5860a661
sunshine111111 <13181711802@163.com>
2022-11-07 16:48:44 +0800

    文本相似度分析例子

--- test/demandSimilarity.py
+++ test/demandSimilarity.py
@@ -19,13 +19,16 @@ dataDemand['demandDescription']=dataDemand['demandDescription'].fillna('')
 #需求内容(考虑到需求描述不完善的情况下，将需求标题考虑进去)
 dataDemand['demandContent']=dataDemand['demandTitle']+dataDemand['demandDescription']
 
-model = SBert('paraphrase-multilingual-MiniLM-L12-v2')
+#model = SBert('paraphrase-multilingual-MiniLM-L12-v2')
+##这里考虑到模型网站访问失败的情况，直接将模型下载到本地使用
+model = SBert('C:\\Users\zhongxing\Desktop\lan\project\paraphrase-multilingual-MiniLM-L12-v2')
 
 #businessScope:单条企业经营范围，demandContents：多条需求信息
-def getDemand(businessScope,demandContents,dataDemandId):
+def getDemand(businessId,businessScope,demandContents,dataDemandId):
     sentencesBusiness = businessScope
     demandSim_total = {}
-    result = []
+    resultItem = []
+    result = {}
     if len(demandContents)>0:
         for id,demandContent in zip(dataDemandId,demandContents):
             sentencesDemand = demandContent
@@ -33,22 +36,25 @@ def getDemand(businessScope,demandContents,dataDemandId):
             embeddingsDemand = model.encode(sentencesDemand)
             cosine_scores = cos_sim(embeddingsBusiness, embeddingsDemand)
             demandSim_total[id]=cosine_scores
-            print(id,cosine_scores.numpy())
+            #print(id,cosine_scores.numpy())
         result_temp = []
         result_temp=sorted(demandSim_total.items(),key=lambda x:x[1],reverse=True)
-        print(result_temp)
+        #print(result_temp)
         if result_temp:
             for item in result_temp:
-                result.append(item[0])
+                resultItem.append(item[0])
         else:
-            result = ["None"]
-    print(result)
+            resultItem = ["None"]
+    print(resultItem)
+    result[businessId]=resultItem
     if out_to_file:
         export=open(resultFile,'w')
-        for item in result:
+        for item in resultItem:
             export.write(str(item)+'\n')
         export.close()
     else:
         return result
-businessScope='许可项目：互联网信息服务；第一类增值电信业务；第二类增值电信业务。（依法须经批准的项目，经相关部门批准后方可开展经营活动，具体经营项目以相关部门批准文件或许可证件为准）一般项目：信息技术咨询服务；技术黄海数据致力于人工智能、区块链、云计算、大数据、5G、产教融合、数字协同、UHD与VR、AR等领域的协同发展。核心业务包括针对数字内容制作领域基于知识图谱的VR、AR、大数据分析、影视后期制作、动漫及'
-getDemand(businessScope,dataDemand['demandContent'],dataDemand['uuid'])
+
+businessScope='玻璃制品加工、销售；玻璃制品购销；普通货物及技术的进出口；废旧玻璃回收。(依法须经批准的项目，经相关部门批准后方可开展经营活动)'
+businessId='b12'
+getDemand(businessId,businessScope,dataDemand['demandContent'],dataDemand['uuid'])
